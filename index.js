41405ce943fb9cf7e6cdb3e26e9fbcc4321f5505
Lenin Rocha <lenin0555@hotmail.com>
2023-05-02 14:31:20 -0300

    new

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 jQuery(function(){
 
-    var xingamentos = [ "isaac vai tomar no cu", "vai se fuder isaac", "cala a boca isaac"]
-    var fotos = ["image-1.jpg", "image-2.jpg", "image-3.jpg"]
-
-    var xingamentoAleatorio = Math.floor(Math.random() * xingamentos.length)
-    var fotoAleatoria = Math.floor(Math.random() * fotos.length)
-    setTimeout(function(){
-        $(".image img").attr("src", "./src/"+fotos[fotoAleatoria])
-        $(".texto").html(`<p>${xingamentos[xingamentoAleatorio]}</p>`)
-    }, 50)
+    $("body").on("click", "#gerar", function(){
+        gerar()
+    })
     
+    function gerar(){
+        var xingamentos = [ "isaac vai tomar no cu", "vai se fuder isaac","Isaac é tão lixo que se o SLU passar leva ele", "cala a boca isaac", "Pau no cu do isac", "vai gravar caralho","Bom almoço a todos os trabalhadores do grupo, menos pro Isaac pq ele é vagabundo, Vai tomar no cu desgraça", " Isaac pega no meu pau"]
+        var fotos = ["image-1.jpg", "image-2.jpg", "image-3.jpg", "image-4.jpeg", "image-5.jpeg", "image-6.jpeg", "image-7.jpeg", "image-8.jpeg", "image-9.jpeg"]
+    
+        var xingamentoAleatorio = Math.floor(Math.random() * xingamentos.length)
+        var fotoAleatoria = Math.floor(Math.random() * fotos.length)
+        setTimeout(function(){
+            $(".image img").attr("src", "./src/"+fotos[fotoAleatoria])
+            $(".texto").html(`<p>${xingamentos[xingamentoAleatorio]}</p>`)
+            $(".image").show()
+        }, 50)
+    }
 })
